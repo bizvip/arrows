39c0ba495aae4b5513f41c38bd0703c7d17e8655
Archer++ <38232156+bizvip@users.noreply.github.com>
2024-02-21 02:11:39 +0700

    init

--- src/Logger.php
+++ src/Logger.php
@@ -5,6 +5,7 @@
  ******************************************************************************/
 
 declare(strict_types=1);
+
 namespace Arrows;
 
 use DateTimeImmutable;
@@ -26,8 +27,8 @@ final class Logger
         $chan = $args[1] ?? $args['channel'] ?? config('app_env');
         $text = $args[0] ?? $args['logData'] ?? '没有任何错误消息？';
         $text = is_array($text) ? JSON::encode($text) : (string)$text;
-        $msg  = sprintf("%s  %s \n", (new DateTimeImmutable())->format('Y-m-d H:i:s.v'), $text);
-        $c    = ApplicationContext::getContainer();
+        $msg = sprintf("%s  %s \n", (new DateTimeImmutable())->format('Y-m-d H:i:s.v'), $text);
+        $c = ApplicationContext::getContainer();
         $c->get(StdoutLoggerInterface::class)->{$method}($msg);
         $c->get(LoggerFactory::class)->get($chan)->{$method}($msg);
     }
